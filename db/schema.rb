b09a7768a825eb96a4023d3694e053b7e7d85f0c
Viksit Arora <viksit@mit.edu>
2016-01-12 12:02:43 +0530

    generates user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112063101) do
+ActiveRecord::Schema.define(version: 20160112063203) do
 
   create_table "authentication_tokens", force: :cascade do |t|
     t.string   "body"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20160112063101) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
